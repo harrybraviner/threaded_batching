59b63f6c16f3dc7bc90f2a1d5b588ef1c893cc62
Harry Braviner <harry@layer6.ai>
2019-08-30 09:34:34 -0400

    Renamed to ThreadedSource, since it's more general than just batching stuff

--- threaded_source.py
+++ threaded_source.py
@@ -2,16 +2,16 @@ from queue import Queue, Empty
 from threading import Lock, Thread
 from abc import ABC, abstractmethod
 
-class ThreadedBatcher(ABC):
-    
+class ThreadedSource(ABC):
+
     def __init__(self, num_enqueueing_threads=2, max_queue_size=10):
         self._num_enqueueing_threads = num_enqueueing_threads
-        
+    
         self._batch_queue = Queue(maxsize=max_queue_size)
-        
+
         # This lock is used to ensure that only one consumer is dequeue-ing at any one time.
         self._iteration_lock = Lock()
-        
+
         # This lock is used to ensure that only one thread is being assigned it batch params at any one time.
         self._batch_params_lock = Lock()
 
@@ -30,7 +30,7 @@ class ThreadedBatcher(ABC):
                                   for _ in range(self._num_enqueueing_threads)]
         for thread in self._queueing_threads:
             thread.start()
-            
+
     def __iter__(self):
         self._reset_iteration()
         while True:
@@ -38,7 +38,7 @@ class ThreadedBatcher(ABC):
                 yield self._next()
             except StopIteration:
                 break
-        
+
     def _next(self):
         with self._iteration_lock: # Ensure that we are the only thread running this function
             if self._data_exhausted is False:
@@ -61,11 +61,11 @@ class ThreadedBatcher(ABC):
     @abstractmethod
     def _get_next_batch_params(self):
         raise NotImplementedError
-    
+
     @abstractmethod
     def _get_batch_from_params(self, params):
-        raise NotImplementedError        
-            
+        raise NotImplementedError
+
     def _enqueue(self):
         """
         Function that gets run its own thread.
@@ -79,3 +79,4 @@ class ThreadedBatcher(ABC):
             else:
                 this_batch = self._get_batch_from_params(batch_params)
                 self._batch_queue.put(this_batch, block=True, timeout=None)
+
